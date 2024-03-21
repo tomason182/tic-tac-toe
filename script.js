c23981836320edafba620523807a9c923b86265e
tomason182 <elotro182@hotmail.com>
2024-03-21 12:54:45 -0300

    add gameBoard Function

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
+function gameBoard () {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
 
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    }
+
+    const getBoard = () => board;
+
+    const printBoard = () => {
+        getCellsValues =  board.map((row) => row.map((cell) => cell.getValue()))
+    }
+}
+
+
+function Cell() {
+
+    
+}
 
 
 
